7c9a343d1d7f06f1325ade80be59acbd06eb6906
Dhanush P N <97773473+l1v3or3v1l@users.noreply.github.com>
2023-08-21 19:32:33 +0530

    Update histogram_transcript.py

--- histogram_transcript.py
+++ histogram_transcript.py
@@ -14,6 +14,7 @@ def histogram(l: list) -> list:
 			tmp = nl[x];
 			nl[x] = nl[x+1];
 			nl[x+1] = tmp;
+	nl.sort();
 	nl.sort(key=(lambda e: e[1]));
 	return nl;
 
@@ -30,6 +31,7 @@ def transcript(coursedetails: list, studentdetails: list, grades: list) -> list:
 			for y in det:
 				if x[0] == y[0]:
 					findet.append((y[0], x[1], y[1]));
+		findet.sort();
 		final.append((student[0], student[1], findet));
 	return final
 
@@ -39,4 +41,4 @@ def transcript(coursedetails: list, studentdetails: list, grades: list) -> list:
 #print(histogram([13,7,12,7,11,13,14,13,7,11,13,14,12,14,14,7]));
 
 #print(transcript([("MA101","Calculus"),("PH101","Mechanics"),("HU101","English")],[("UGM2021001","Rohit Grewal"),("UGP2021132","Neha Talwar")],[("UGM2021001","MA101","AB"),("UGP2021132","PH101","B"),("UGM2021001","PH101","B")]));
-#print(transcript([("T1","Test 1"),("T2","Test 2"),("T3","Test 3")],[("Captain","Rohit Sharma"),("Batsman","Virat Kohli"),("No3","Cheteshwar Pujara")],[("Batsman","T1","14"),("Captain","T1","33"),("No3","T1","30"),("Batsman","T2","55") ,("Captain","T2","158"),("No3","T2","19"), ("Batsman","T3","33"),("Captain","T3","95"),("No3","T3","51")]));
+#print(transcript([("T2","Test 2"),("T1","Test 1"),("T3","Test 3")],[("Captain","Rohit Sharma"),("Batsman","Virat Kohli"),("No3","Cheteshwar Pujara")],[("Batsman","T1","14"),("Captain","T1","33"),("No3","T1","30"),("Batsman","T2","55") ,("Captain","T2","158"),("No3","T2","19"), ("Batsman","T3","33"),("Captain","T3","95"),("No3","T3","51")]));
