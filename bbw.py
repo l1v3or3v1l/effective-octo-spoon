dd18dc0684dc05c3c4e20c08d0a757bc8db0fbcf
Dhanush P N <97773473+l1v3or3v1l@users.noreply.github.com>
2024-05-10 11:29:32 +0530

    big bandwidth waster hehe

---
+++ bbw.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import time
+import urllib.request
+
+
+def progress(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r %d MB, %d KB/s, %d seconds passed" %
+                    (progress_size / (1024 * 1024), speed, duration))
+    sys.stdout.flush()
+
+def save():
+	while True:
+		urllib.request.urlretrieve("https://releases.ubuntu.com/24.04/ubuntu-24.04-desktop-amd64.iso", os.devnull, progress)
+
+if __name__ == '__main__':
+	save()
