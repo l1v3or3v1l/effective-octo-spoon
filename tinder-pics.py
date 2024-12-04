00bb666384384e56e6deca9e655278007f186cd0
Dhanush P N <97773473+l1v3or3v1l@users.noreply.github.com>
2024-12-04 11:48:55 +0530

    Update tinder-pics.py

--- tinder-pics.py
+++ tinder-pics.py
@@ -3,25 +3,20 @@
 # python tinder-pics.py
 
 import requests
-import re
-import json
-from bs4 import BeautifulSoup
-from sys import exit
-import wget
-import os
+import re                                            import json                                          from bs4 import BeautifulSoup                        from sys import exit
+import wget                                          import os
 
 url = 'https://tinder.com/@'
 
 xx = input('username : ')
 
-r = requests.get(url+xx)
-if f'/@{xx}' not in r.text:
-	exit()
+r = requests.get(url+xx)                             if f'/@{xx}' not in r.text:
+        exit()
 
 soup = BeautifulSoup(r.text, 'html.parser')
-script = soup.find('script', text=re.compile('window\.__data'))
+script = soup.find('script', string=re.compile('window.__data'))
 
-json_text = re.search(r'^\s*window\.__data\s*=\s*({.*?})\s*;\s*$',
+json_text = re.search(r'^\s*window.__data\s*=\s*({.*?})\s*;\s*$',
                       script.string, flags=re.DOTALL | re.MULTILINE).group(1)
 
 data = json.loads(json_text)
@@ -31,8 +26,8 @@ parr = data['webProfile']['user']['photos']
 os.mkdir(xx)
 
 for p in parr:
-	purl = p['processedFiles'][0]['url']
-	pr = requests.get(purl, allow_redirects=True)
-	open(f'{xx}/{purl.split("_")[-1]}', 'wb').write(pr.content)
+        purl = p['processedFiles'][0]['url']
+        pr = requests.get(purl, allow_redirects=True)
+        open(f'{xx}/{purl.split("_")[-1]}', 'wb').write(pr.content)
 
 print(url+xx)
