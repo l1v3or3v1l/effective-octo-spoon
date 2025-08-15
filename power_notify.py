f3809e1925c75312828e376f6ebae34aa4411d34
Dhanush P N <97773473+l1v3or3v1l@users.noreply.github.com>
2025-08-15 12:36:03 +0530

    no more AV false flag

--- power_notify.py
+++ power_notify.py
@@ -1,26 +1,48 @@
-import time
 import psutil
 import winsound
 from winotify import Notification, audio
+from PIL import Image, ImageDraw
+import pystray
+
+
+def create_image():
+    img = Image.new('RGB', (64, 64), color=(255, 255, 0))
+    d = ImageDraw.Draw(img)
+    d.rectangle([16, 16, 48, 48], fill=(0, 0, 0))
+    return img
+
 
 def check_power():
     battery = psutil.sensors_battery()
-    while battery and not battery.power_plugged:
-        battery = psutil.sensors_battery()
-        time.sleep(5)
+    if battery and not battery.power_plugged:
         alert()
+
 
 def alert():
     winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-
     toast = Notification(
         app_id="Power Alert",
         title="⚠ Laptop Unplugged!",
         msg="Your laptop is running on battery.",
-        duration="short"
+        duration="long"
     )
     toast.set_audio(audio.Default, loop=False)
     toast.show()
 
+
+def on_exit(icon, item):
+    icon.stop()
+
+
 if __name__ == '__main__':
-    check_power()
+    menu = pystray.Menu(
+        pystray.MenuItem("Exit", on_exit)
+    )
+
+    icon = pystray.Icon("Power Alert", create_image(), "Power Alert", menu)
+
+    def setup(icon):
+        check_power()
+        icon.stop()
+
+    icon.run(setup)
